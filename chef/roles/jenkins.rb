1334fccaf156caab7a1106c8a62cb0a68dc4740c
Touqeer Hussain <touqeer.hussain@hotmail.co.uk>
2015-12-10 11:14:08 +0000

    updating jenkins.rb

--- chef/roles/jenkins.rb
+++ chef/roles/jenkins.rb
@@ -1,3 +1,3 @@
 name "jenkins"
 description "CD/CI servers"
-run_list "recipe[iptables::disabled]","recipe[ntp]","recipe[sshkeys::default]","recipe[golang::packages]","recipe[jenkins::java]","recipe[jenkins::master]","recipe[jenkins::user]"
+run_list "recipe[iptables::disabled]","recipe[ntp]","recipe[sshkeys-jenkins::default]","recipe[golang::packages]","recipe[jenkins::java]","recipe[jenkins::master]","recipe[jenkins::config]"
